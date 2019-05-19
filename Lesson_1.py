d5230fe8cda45f902645cff890f27eea46c0e726
fitc293 <50141366+fitc293@users.noreply.github.com>
2019-05-19 21:35:13 +0300

    Update Lesson_1.py

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,3 +1,11 @@
 # 1. Даны 2 строки long_phrase и short_phrase. 
 # Напишите код, который проверяет действительно ли длинная фраза long_phrase длиннее короткой short_phrase. 
 # И выводит True или False в зависимости от результата сравнения.
+
+long_phrase = 'Насколько проще было бы писать программы, если бы не заказчики'
+short_phrase = '640Кб должно хватить для любых задач. Билл Гейтс (по легенде)'
+
+if len(long_phrase) > len(short_phrase):
+    print(True)
+else:
+    print(False)
